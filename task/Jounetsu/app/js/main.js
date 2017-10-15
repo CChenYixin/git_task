0efaba749567302d8262880aac2da589792e30a7
chenyixin <onepiececee@163.com>
2017-10-15 21:00:48 +0800

    add new file

--- task/Jounetsu/app/js/main.js
+++ task/Jounetsu/app/js/main.js
@@ -29,7 +29,7 @@ function $(selector){
 // });
 
 var xhr = new XMLHttpRequest();
-xhr.open('get', '/html/music.json', true);
+xhr.open('get', 'http://chenyixin.win/git_task/task/Jounetsu/app/html/index.html', true);
 xhr.send();
 xhr.onload = function() {
     if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
