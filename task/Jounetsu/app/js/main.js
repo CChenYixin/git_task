d480fd576367d2d8f6fa0fd6c8acaf1e3bb25063
陈一欣 <onepiececee@163.com>
2017-10-15 21:10:10 +0800

    Update main.js

--- task/Jounetsu/app/js/main.js
+++ task/Jounetsu/app/js/main.js
@@ -10,24 +10,6 @@ function $(selector){
  * 对封装好的ajax进行调用
  */
 
-// ajax({
-//     url:'/html/music.json',
-//     type:'GET',
-//     // data:{
-//     //     name:'chenyixin'
-//     // },
-//     dataType:'JSON',
-//     async:true,
-//     success:function(response,xml){
-//         console.log(JSON.parse(response));
-//         var list = JSON.parse(response);
-//         getMusicList(list);
-//     },
-//     fail:function(status){
-//         console.log('获取数据失败，状态码为'+status);
-//     }
-// });
-
 var xhr = new XMLHttpRequest();
 xhr.open('get', '/html/music.json', true);
 xhr.send();
